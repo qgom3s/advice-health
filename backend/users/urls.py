aaa1cd7377b6b0be93d6f2c1806eaaf79f2b9cf8
QGom3s <81332306+qgom3s@users.noreply.github.com>
2025-07-19 18:20:23 -0300

    :bug: Bug de authenticação resolvido.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, UserRegisterView
 
 router = DefaultRouter()
-router.register(r'', UserViewSet, basename='users')
+router.register(r'', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register/', UserRegisterView.as_view(), name='register'),
-]
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+] + router.urls
